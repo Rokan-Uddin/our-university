45ca261dd44777db8a018d4988a53084841a383a
Rokan Uddin <rokanu55@gmail.com>
2021-12-23 00:51:55 +0600

    student added to database

--- src/components/AddStudent/AddStudent.js
+++ src/components/AddStudent/AddStudent.js
@@ -1,11 +1,23 @@
+import axios from 'axios';
 import React from 'react';
-
+import { useForm } from "react-hook-form";
 const AddStudent = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => {
+      axios.post("https://sheltered-crag-88066.herokuapp.com/addstudent",data)
+      .then(res=>{
+          console.log(res);
+      })
+  };
     return (
-        <div>
-            <h1>Add Students</h1>
-        </div>
-    );
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input  {...register("name", { required: true })} />
+          {errors.name && <span>This field is required</span>}
+          <input {...register("dept", { required: true })} />
+          {errors.dept && <span>This field is required</span>}
+          <input type="submit" />
+        </form>
+      );
 };
 
 export default AddStudent;
